b784211ab684d221a13831abd5e1cb4e1eba1061
Jacob <jdandreas@gmail.com>
2017-01-25 14:07:22 -0800

    new color

--- src/tasks/color.py
+++ src/tasks/color.py
@@ -1,7 +1,10 @@
 from data.color import munroecorpus
 from ref import RefTask
 
+import cairo
+import colorsys
 import numpy as np
+import os
 
 class ColorRefTask(RefTask):
     def __init__(self):
@@ -40,6 +43,8 @@ class ColorRefTask(RefTask):
         self.n_examples = len(self.colors)
         self.n_vocab = len(self.vocab)
 
+        self.image_counter = 0
+
     def get_pair(self, fold):
         i1, i2 = self.random.randint(self.n_examples, size=2)
         return self.colors[i1, :], self.colors[i2, :], self.sentences[i1], None, None
@@ -58,6 +63,32 @@ class ColorRefTask(RefTask):
         if agent == 0 and state.target == 1:
             block1, block2 = block2, block1
         return block1 + "\n" + block2
+
+    def turk_visualize(self, state, agent, loc):
+        data1 = np.zeros((200, 200, 4), dtype=np.uint8)
+        surf1 = cairo.ImageSurface.create_for_data(data, cairo.FORMAT_ARGB32, 200, 200)
+        ctx1 = cairo.Context(surf1)
+        rgb1 = colorsys.hls_to_rgb(state.left[0], state.left[2], state.left[1])
+        ctx1.set_source_rgb(*rgb1)
+        ctx1.fill()
+
+        data2 = np.zeros((200, 200, 4), dtype=np.uint8)
+        surf2 = cairo.ImageSurface.create_for_data(data, cairo.FORMAT_ARGB32, 200, 200)
+        ctx2 = cairo.Context(surf1)
+        rgb2 = colorsys.hls_to_rgb(state.right[0], state.right[2], state.right[1])
+        ctx2.set_source_rgb(*rgb2)
+        ctx1.fill()
+
+        if agent == 0 and state.target == 1:
+            surf1, surf2 = surf2, surf1
+
+        name1 = "%d_a.png" % self.image_counter
+        name2 = "%d_b.png" % self.image_counter
+
+        surf1.write_to_png(os.path.join(loc, name1))
+        surf2.write_to_png(os.path.join(loc, name2))
+
+        return name1, name2
 
     def pp(self, indices):
         return " ".join([self.reverse_vocab[i] for i in indices])
